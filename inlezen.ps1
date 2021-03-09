a3ac182a6f1b9b05db65fcab680a113958e8a43d
MrMagic78 <80316604+MrMagic78@users.noreply.github.com>
2021-03-09 09:40:41 +0100

    Add files via upload

---
+++ inlezen.ps1
@@ -0,0 +1,17 @@
+﻿####### Script om IMID.CSV (welke uit uitlezen.ps1 is gekomen) in te lezen in AzureAD.
+####### https://www.microsoft.com/en-us/download/details.aspx?id=54616 = Windows Management Framework 5.1 deze is nodig voor Windows 7, Windows 8.1, Server 2008 R2/2012/2012R2.
+####### Reboot
+####### Install-Module MSOnline
+####### Indien foutmeldingen dan [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+connect-msolservice
+$users = import-csv “c:\temp\IMID.csv”
+ForEach ($item in $users)
+
+{
+$UPN = $item.(“UserPrincipalName”)
+
+$IMID = $item.(“ImmutableID”)
+
+Set-MsolUser -UserPrincipalName $UPN -ImmutableId $IMID
+}
---
+++ uitlezen.ps1
@@ -0,0 +1,41 @@
+﻿####### Script om alle of 1 gebruiker te exporteren naar CSV met ImmutableID. Deze kan daarna worden ingelezen in de AzureAD.
+####### Dit script moet je draaien op een OnPremise DC.
+
+[System.Console]::ForegroundColor = [System.ConsoleColor]::White
+clear-host
+Import-module activedirectory
+
+write-host Kies 1 van de volgende opties:
+write-host
+write-host ‘1) Vraag het ImmutableID op voor 1 gebruiker. Naam die je in moet vullen is sAMAccountName dus zonder @.’
+write-host ‘2) Vraag het ImmutabaleID op voor alle gebruikers.’
+write-host ‘3) Stop’ -ForegroundColor Red
+write-host
+$option = Read-Host “Maak een keuze [1-3]”
+
+switch ($option)
+{
+‘1’{
+write-verbose “Optie 1 geselecteerd.”
+$GetUser = Read-Host -Prompt ‘Vul de gebruikersnaam in:’
+$Users = get-aduser $GetUser | select userprincipalname,@{label=”ImmutableID”;expression={[System.Convert]::ToBase64String($_.objectguid.ToByteArray())}}
+$Users
+}
+
+‘2’{
+Write-Verbose "Optie 2 geselecteerd."
+Write-host
+Write-host 'Vul het pad in waar de export moet komen te staan. Bijv. c:\temp\IMID.csv'
+
+$Path = Read-Host -Prompt ‘Pad’
+
+$Users = get-aduser -filter * | select userprincipalname,@{label=”ImmutableID”;expression={[System.Convert]::ToBase64String($_.objectguid.ToByteArray())}}
+$users
+$users | export-csv $Path
+
+}
+‘3’{
+write-verbose “Optie 3 geselecteerd.”
+break
+}
+}
